27130525d9ad4342cade1a5ccb0cf7bcf8d39e78
Laura Klajn <s27615@pjwstk.edu.pl>
2024-03-12 13:30:40 +0100

    Add average method

--- zadanie_2/zadanie_2/Program.cs
+++ zadanie_2/zadanie_2/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+static double GetAverage(int[] tab)
+{
+    int suma = 0;
+    foreach (var number in tab)
+    {
+        suma += number;
+    }
+    
+    return (double) suma / tab.Length;
+}
